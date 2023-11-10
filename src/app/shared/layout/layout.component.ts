00c2fa95e209d5937878d9b802b2820fa9ab13e5
aghosby <etiboaghogho@gmail.com>
2023-11-10 21:15:52 +0000

    Getting user details on email verification

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -16,49 +16,51 @@ export class LayoutComponent implements OnInit {
   constructor(private router: Router, private auth: AuthenticationService, private notify: NotificationService) { }
 
   ngOnInit(): void {
-    if(this.userDetails.data.firstTimeLogin) {
-      let urlsplit = this.router.url?.split("/dashboard/");
-
-      if(urlsplit.length > 1) {
-        console.log(urlsplit);
-        console.log(JSON.parse(atob(urlsplit[1].split('.')[1])))
-        this.authDetails = JSON.parse(atob(urlsplit[1].split('.')[1]))
-        let info = {
-          token: urlsplit[1]
-        }
-        this.auth.verifyEmail(info).subscribe({
-          next: res => {
-            console.log(res);
-            if(res.status == 200) {
-              this.notify.showSuccess("Your email has been verified");
-              let authInfo = {
-                email: this.authDetails.email,
-                password: this.authDetails.password
-              }
-              this.auth.login(authInfo).subscribe({
-                next: res => {
-                  console.log(res);
-                  if(res.status == 200) {
-                    this.userDetails = res.data;
-                    if(res.data.isSuperAdmin) this.router.navigate(['app/settings']);
-                    else this.router.navigate(['set-password']);
-                  }
-                },
-                error: err => {
-                  console.log(err)
-                  this.notify.showError("err.message");
-                }          
-              })
-            }
-          },
-          error: err => {
-            console.log(err)
-            //this.notify.showError("res.message");
-          }          
-        })
+    let urlsplit = this.router.url?.split("/dashboard/");
+    if(urlsplit.length > 1) {
+      console.log(urlsplit);
+      console.log(JSON.parse(atob(urlsplit[1].split('.')[1])))
+      this.authDetails = JSON.parse(atob(urlsplit[1].split('.')[1]))
+      console.log(this.authDetails);
+      this.userDetails = this.authDetails;
+      let info = {
+        token: urlsplit[1]
       }
-      //console.log(userParam);
-    } 
+      if(this.userDetails.data.firstTimeLogin) {
+        
+        //console.log(userParam);
+      }
+      this.auth.verifyEmail(info).subscribe({
+        next: res => {
+          console.log(res);
+          if(res.status == 200) {
+            this.notify.showSuccess("Your email has been verified");
+            let authInfo = {
+              email: this.authDetails.email,
+              password: this.authDetails.password
+            }
+            this.auth.login(authInfo).subscribe({
+              next: res => {
+                console.log(res);
+                if(res.status == 200) {
+                  this.userDetails = res.data;
+                  if(res.data.isSuperAdmin) this.router.navigate(['app/settings']);
+                  else this.router.navigate(['set-password']);
+                }
+              },
+              error: err => {
+                console.log(err)
+                this.notify.showError("err.message");
+              }          
+            })
+          }
+        },
+        error: err => {
+          console.log(err)
+          //this.notify.showError("res.message");
+        }          
+      })
+    }
     else {
       this.userDetails = this.auth.loggedInUser;
     }
