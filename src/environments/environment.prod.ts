580bbccf5612d7a83772d79685bac244ea153074
Ajuwon Tolu Johnson <47560491+teejohn247@users.noreply.github.com>
2024-11-25 14:22:06 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: "https://api-bk.silo-inc.com",
+  baseUrl: "https://api-bk.silo-inc.com/api/v1",
   siloBaseUrl: "https://erp-bk.onrender.com/api/v1",
   //baseUrl: "http://silo-env-1.eba-agppeqvm.eu-west-1.elasticbeanstalk.com",
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODMzZWE5Mjg0NzY4ZDU1MmJhMmEyYSIsImlzQWRtaW4iOiJ0b2x1Sm9obnNvbjI0QGdtYWlsLmNvbSIsImlhdCI6MTY4NjQyNTc0MSwiZXhwIjoxNjg3Mjg5NzQxfQ.oEtxj3QSHPfpJk1Q1s5FUjM5wSnw4VDscq5VVOMKESw",
