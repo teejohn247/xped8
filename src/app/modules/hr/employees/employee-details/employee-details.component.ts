2af5229aaaedf4c618819fdbab409473435429a4
aghosby <etiboaghogho@gmail.com>
2025-05-08 17:26:45 +0100

    Fixed date issues

--- src/app/modules/hr/employees/employee-details/employee-details.component.ts
+++ src/app/modules/hr/employees/employee-details/employee-details.component.ts
@@ -192,19 +192,31 @@ export class EmployeeDetailsComponent implements OnInit {
 
   }
 
-
-  strToDate(dateVal: string, isDate:boolean) {
-    if(isDate) {
-      let newFormat = new Date(dateVal);
-      return this.datePipe.transform(newFormat, 'd MMMM, y')
+  strToDate(dateVal: string) {
+    if(dateVal != 'undefined') {
+      let reqDate:any = new Date(dateVal)
+      if(reqDate == 'Invalid Date' && dateVal != 'undefined') {
+        const [day, month, year] = dateVal.split('-');
+        let newFormat = new Date(+year, +month - 1, +day);
+        reqDate = newFormat;
+      }
+      return reqDate;
     }
-    else {
-      const [day, month, year] = dateVal.split('-');
-      let newFormat = new Date(+year, +month - 1, +day);
-      // console.log(newFormat.toDateString());
-      return this.datePipe.transform(newFormat, 'd MMMM, y')
-    }    
-  } 
+  }
+
+
+  // strToDate(dateVal: string, isDate:boolean) {
+  //   if(isDate) {
+  //     let newFormat = new Date(dateVal);
+  //     return this.datePipe.transform(newFormat, 'd MMMM, y')
+  //   }
+  //   else {
+  //     const [day, month, year] = dateVal.split('-');
+  //     let newFormat = new Date(+year, +month - 1, +day);
+  //     // console.log(newFormat.toDateString());
+  //     return this.datePipe.transform(newFormat, 'd MMMM, y')
+  //   }    
+  // } 
 
   editEmployeeInfo() {
     let dialogRef = this.dialog.open(EditEmployeeComponent, {
