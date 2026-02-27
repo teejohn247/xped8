9416760fcbe2252a907cf35d28e96833bae94d2b
Ajuwon Tolu Johnson <47560491+teejohn247@users.noreply.github.com>
2026-02-27 09:36:40 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   //baseUrl: "https://main.dv3qs412pu5vf.amplifyapp.com",
   siloBaseUrl: "https://erp-bk.onrender.com/api/v1",
-  baseUrl: "https://erp-bk-staging-766078353087.us-central1.run.app/api/v1",
+  baseUrl: "https://erp-bk-766078353087.europe-west1.run.app/api/v1",
   token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODMzZWE5Mjg0NzY4ZDU1MmJhMmEyYSIsImlzQWRtaW4iOiJ0b2x1Sm9obnNvbjI0QGdtYWlsLmNvbSIsImlhdCI6MTY4NjQyNTc0MSwiZXhwIjoxNjg3Mjg5NzQxfQ.oEtxj3QSHPfpJk1Q1s5FUjM5wSnw4VDscq5VVOMKESw",
   token2: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0N2I5YzcwMzQxMjE3ODg2MTZkMzhiMiIsImlzQWRtaW4iOiJkZXZAc2lsby1pbmMuY29tIiwiaWF0IjoxNjg3MDIyNzkyLCJleHAiOjE2ODc4ODY3OTJ9.HW0usTCRMLJzcg6q8cQdg9Nq1FxhYQxwPt1e8ZnBUSQ"
 };
